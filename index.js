559fe64dfd5545b79c71d46961dc56661ab657a6
MushroomFX <dergolem26@gmail.com>
2022-10-30 01:49:54 +0200

    - i hope i fixed it finnaly

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-function skyPlus(){
 var timeout =setTimeout(function() {
 	for (i = 0;i < document.querySelectorAll(".max-stat > .stat-name").length;i++) {
 		var temp = document.querySelectorAll(".max-stat > .stat-name")[i].innerText;
@@ -72,5 +71,4 @@ function toggleFull(){
 		document.getElementById('full_button').style.background = "#ffffffcc"
 	}
 	console.log(localStorage.getItem('fullMode'));
-}
 }
